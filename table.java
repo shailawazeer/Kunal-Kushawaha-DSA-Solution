0e7f873ecdc04735f6a3d1d7c9271fd286f281e9
shailawazeer <sshailafeb@gmail.com>
2025-12-23 19:00:42 +0500

    3 question

---
+++ table.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class table {
+public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the number for table:");
+    int num1 = input.nextInt();
+    for(int i=1; i<=10; i++)
+    System.out.println(num1 + " * " + i + " = " + num1 * i);
+    
+    }
+    
+}
